689f64fe8c03dafd7744b31e2a3081299e415eae
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com>
2011-09-13 22:49:17 +0000

    typo

--- start MMMR console.bat
+++ start MMMR console.bat
@@ -1,4 +1,4 @@
 @FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Runtime Environment" /v CurrentVersion') DO @SET CurrentVersion=%%B
 @FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Runtime Environment\%CurrentVersion%" /v JavaHome') DO @SET JavaHome=%%B
-"%JavaHome%\bin\java.exe" -jar mmmr*.bat -console=true
+"%JavaHome%\bin\java.exe" -jar mmmr*.jar -console=true
 @pause
