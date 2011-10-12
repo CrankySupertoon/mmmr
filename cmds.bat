d154708520ff345b8a3a57ad22cf4a9877334876
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com>
2011-10-12 15:24:30 +0000

    replaced by 1 bat file

--- cmds.bat
+++ cmds.bat
@@ -1,40 +1,53 @@
 @echo off
 cls
 :start
+echo ------------------
 echo 1. update code
 echo 2. clean target
 echo 3. create package
 echo 4. build site
 echo 5. deploy
-echo 6. quit
-CHOICE /C:123456
+echo 6. clear screen
+echo 7. quit
+echo ------------------
+CHOICE /N /C:1234567
 set nr=%ERRORLEVEL%
-IF %nr% EQU 1 goto l1
-IF %nr% EQU 2 goto l2
-IF %nr% EQU 3 goto l3
-IF %nr% EQU 4 goto l4
-IF %nr% EQU 5 goto l5
-IF %nr% EQU 6 goto end
-:l1
-echo 1
+IF %nr% EQU 1 goto lupdate
+IF %nr% EQU 2 goto lclean
+IF %nr% EQU 3 goto lpackage
+IF %nr% EQU 4 goto lsite
+IF %nr% EQU 5 goto ldeploy
+IF %nr% EQU 6 goto lcls
+IF %nr% EQU 7 goto end
+
+:lupdate
+cls
 svn update
 goto start
-:l2
-echo 2
-mvn clean
+
+:lclean
+cls
+call mvn clean
 goto start
-:l3
-echo 3
-mvn package
+
+:lpackage
+cls
+call mvn package
 goto start
-:l4
-echo 4
-mvn site
+
+:lsite
+cls
+call mvn site
 goto start
-:l5
-echo 5
-mvn deploy
+
+:ldeploy
+cls
+call mvn deploy
 goto start
+
+:lcls
+cls
+goto start
+
 :end
-echo end
 exit
